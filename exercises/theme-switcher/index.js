40413fbc1da2bb9185a98821b6e83c30610ddc40
Ohudalshammari <46870445+Ohudalshammari@users.noreply.github.com>
2019-03-05 18:39:55 +0300

    HW-W6,D2
    Done!

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,21 @@
 // click event for gray button
-
+$("#grayButton").on ("click", switchGray);
 // click event for white button
-
+$("#whiteButton").on ("click",switchWhite);
 function switchGray() {
     // change background to gray
     // change font to white
+    let body = $("body");
+    console.log(body);
+    body.css("background","gray");
+    body.css("color","white");
 }
 
 function switchWhite() {
     // change background to white
     // change font to black
+    let body = $("body");
+    console.log(body);
+    body.css("background","white");
+    body.css("color","black");
 }
